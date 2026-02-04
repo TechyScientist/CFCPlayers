2015a4bb167fbac38bdbf2954ae6bb9d652b0bb1
Johnny Console <johnny.console@hotmail.com>
2026-02-03 21:53:59 -0500

    Migrate PlayerDetailsActivity to new Material standards

--- Android/app/src/main/java/net/johnnyconsole/cfcplayers/PlayerDetailsActivity.kt
+++ Android/app/src/main/java/net/johnnyconsole/cfcplayers/PlayerDetailsActivity.kt
@@ -1,11 +1,13 @@
 package net.johnnyconsole.cfcplayers
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import net.johnnyconsole.cfcplayers.databinding.ActivityPlayerDetailsBinding
 import net.johnnyconsole.cfcplayers.objects.Player
 import java.text.SimpleDateFormat
@@ -19,94 +21,95 @@ class PlayerDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        player = intent.getSerializableExtra("profile") as Player
-        binding.title.text = getString(R.string.PlayerDetails, player.cfcId, player.name)
+        with(binding) {
+            setContentView(root)
+            enableEdgeToEdge()
 
-        binding.cfcID.text = getString(R.string.placeholderInt, player.cfcId)
-        binding.name.text = getString(R.string.placeholderString, player.name)
-
-        if(player.expiry.isEmpty() || player.expiry.isBlank()) {
-            binding.expiry.text = getString(R.string.noExpiry)
-        }
-        else if(player.expiry.isNotEmpty() && player.expiry.isNotBlank()) {
-            val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.CANADA)
-            val expiry = formatter.parse(player.expiry)
-            val today = formatter.parse(formatter.format(Date()))
-
-            if(expiry != null && today != null && expiry.year >= today.year + 5)  {
-                binding.expiry.text = getString(R.string.noExpiry)
+            ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+                insets
             }
-            else if(expiry != null && today != null && expiry <= today) {
-                binding.expiry.text = getString(R.string.expired, player.expiry)
-                binding.expiry.setTextColor(getColor(R.color.CFCRed))
-            }
-            else {
-                binding.expiry.text = getString(R.string.placeholderString, player.expiry)
-            }
-        }
+            player = intent.getSerializableExtra("profile") as Player
+            title.text = getString(R.string.PlayerDetails, player.cfcId, player.name)
 
-        binding.cityProv.text = getString(R.string.placeholderString, player.cityProvince)
+            cfcID.text = getString(R.string.placeholderInt, player.cfcId)
+            name.text = getString(R.string.placeholderString, player.name)
 
-        if(player.fideID > 0) {
-            binding.fideID.text = getString(R.string.placeholderInt, player.fideID)
-        }
-        else {
-            binding.fideID.text = getString(R.string.unregistered)
-            binding.fideID.setTextColor(getColor(R.color.CFCRed))
-        }
+            if (player.expiry.isEmpty() || player.expiry.isBlank()) {
+                expiry.text = getString(R.string.noExpiry)
+            } else if (player.expiry.isNotEmpty() && player.expiry.isNotBlank()) {
+                val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.CANADA)
+                val expiryDate = formatter.parse(player.expiry)
+                val today = formatter.parse(formatter.format(Date()))
 
-        if(player.regular > 0) {
-             binding.regular.text = getString(R.string.placeholderInt, player.regular)
-        }
-        else {
-             binding.regular.text = getString(R.string.unrated)
-             binding.regular.setTextColor(getColor(R.color.CFCRed))
-        }
-
-        if(player.quick > 0) {
-            binding.quick.text = getString(R.string.placeholderInt, player.quick)
-        }
-        else {
-            binding.quick.text = getString(R.string.unrated)
-            binding.quick.setTextColor(getColor(R.color.CFCRed))
-        }
-
-        binding.updated.text = getString(R.string.placeholderString, player.updated)
-
-        binding.cfcID.setOnClickListener {_ ->
-           val url = if(Locale.getDefault().language.contains("en")) {
-                "https://chess.ca/en/ratings/p/?id=${player.cfcId}"
-            }
-            else {
-                "https://chess.ca/fr/ratings/p/?id=${player.cfcId}"
+                if (expiryDate != null && today != null && expiryDate.year >= today.year + 5) {
+                    expiry.text = getString(R.string.noExpiry)
+                } else if (expiryDate != null && today != null && expiryDate <= today) {
+                    expiry.text = getString(R.string.expired, player.expiry)
+                    expiry.setTextColor(getColor(R.color.CFCRed))
+                } else {
+                    expiry.text = getString(R.string.placeholderString, player.expiry)
+                }
             }
 
-            val intent = Intent(this, ViewPlayerWebProfileActivity::class.java)
-            intent.putExtra("url", url)
-            intent.putExtra("profile", getString(R.string.cfc))
-            startActivity(intent)
-        }
+            cityProv.text = getString(R.string.placeholderString, player.cityProvince)
 
-        binding.fideID.setOnClickListener {_ ->
-            if(player.fideID == 0) {
-                val msgbox = AlertDialog.Builder(this).setNeutralButton(R.string.dismiss, null)
-                    .setTitle(R.string.notFideRegisteredTitle)
-                    .setMessage(R.string.notFideRegisteredMessage)
-                    .create()
-                msgbox.show()
-
-                (msgbox.getButton(AlertDialog.BUTTON_NEUTRAL).layoutParams as LinearLayout.LayoutParams).width = LinearLayout.LayoutParams.MATCH_PARENT
+            if (player.fideID > 0) {
+                fideID.text = getString(R.string.placeholderInt, player.fideID)
+            } else {
+                fideID.text = getString(R.string.unregistered)
+                fideID.setTextColor(getColor(R.color.CFCRed))
             }
-            else {
-                val url = "https://ratings.fide.com/profile/${player.fideID}"
-                val intent = Intent(this, ViewPlayerWebProfileActivity::class.java)
+
+            if (player.regular > 0) {
+                regular.text = getString(R.string.placeholderInt, player.regular)
+            } else {
+                regular.text = getString(R.string.unrated)
+                regular.setTextColor(getColor(R.color.CFCRed))
+            }
+
+            if (player.quick > 0) {
+                quick.text = getString(R.string.placeholderInt, player.quick)
+            } else {
+                quick.text = getString(R.string.unrated)
+                quick.setTextColor(getColor(R.color.CFCRed))
+            }
+
+            updated.text = getString(R.string.placeholderString, player.updated)
+
+            cfcID.setOnClickListener { _ ->
+                val url = if (Locale.getDefault().language.contains("en")) {
+                    "https://chess.ca/en/ratings/p/?id=${player.cfcId}"
+                } else {
+                    "https://chess.ca/fr/ratings/p/?id=${player.cfcId}"
+                }
+
+                val intent = Intent(this@PlayerDetailsActivity, ViewPlayerWebProfileActivity::class.java)
                 intent.putExtra("url", url)
-                intent.putExtra("profile", "FIDE")
+                intent.putExtra("profile", getString(R.string.cfc))
                 startActivity(intent)
+            }
+
+            fideID.setOnClickListener { _ ->
+                if (player.fideID == 0) {
+                    val msgbox = AlertDialog.Builder(this@PlayerDetailsActivity).setNeutralButton(R.string.dismiss, null)
+                        .setTitle(R.string.notFideRegisteredTitle)
+                        .setMessage(R.string.notFideRegisteredMessage)
+                        .create()
+                    msgbox.show()
+
+                    (msgbox.getButton(AlertDialog.BUTTON_NEUTRAL).layoutParams as LinearLayout.LayoutParams).width =
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                } else {
+                    val url = "https://ratings.fide.com/profile/${player.fideID}"
+                    val intent = Intent(this@PlayerDetailsActivity, ViewPlayerWebProfileActivity::class.java)
+                    intent.putExtra("url", url)
+                    intent.putExtra("profile", "FIDE")
+                    startActivity(intent)
+                }
             }
         }
     }
-
 }
