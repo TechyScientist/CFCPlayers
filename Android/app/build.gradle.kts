9611a9668d28e49ca3de1e1159b0c49fb47736d1
Johnny Console <johnny.console@hotmail.com>
2026-02-03 21:30:29 -0500

    Add Kotlin Coroutine dependency

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -35,10 +35,10 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.kotlinx.coroutines)
 }
